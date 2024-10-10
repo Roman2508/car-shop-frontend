cadd2224d966a63370c9d6e95fdc72761c115590
Roman Ptashnyk <2508romaa@example.com>
2024-10-10 22:39:37 +0300

    filter

--- components/modules/CatalogPage/CatalogFiltersDesktop.tsx
+++ components/modules/CatalogPage/CatalogFiltersDesktop.tsx
@@ -11,90 +11,10 @@ import { $mode } from '@/context/mode'
 import FilterManufacturerAccordion from './FilterManufacturerAccordion'
 import Accordion from '@/components/elements/Accordion/Accordion'
 import PriceRange from './PriceRange'
-import { ICatalogFilterDesktopProps, IFilterCheckboxItem } from '@/types/catalog'
-import spinnerStyles from '@/styles/spinner/index.module.scss'
 import styles from '@/styles/catalog/index.module.scss'
-
-// Лакове покриття
-const varnishCoating = [
-  { checked: false, id: '1', title: 'Як нове, без видимих слідів експлуатації', event: '' },
-  { checked: false, id: '2', title: 'Професійно відремонтоварі сліди експлуатації', event: '' },
-  { checked: false, id: '3', title: 'Незначні сліди експлуатації (дрібні подряпини, сколи)', event: '' },
-  { checked: false, id: '4', title: "Не відремонтовані сліди експлуатації (подряпини, вм'ятини і т.д.)", event: '' },
-  { checked: false, id: '5', title: 'Потрібно відновлення (фарбування, заміна деталей, зварювання)', event: '' },
-] as unknown as IFilterCheckboxItem[]
-
-// Технічний стан
-const technicalCondition = [
-  { checked: false, id: '1', title: 'На ходу, технічно справна', event: '' },
-  { checked: false, id: '2', title: 'Не бита', event: '' },
-  { checked: false, id: '3', title: 'Не пофарбована', event: '' },
-  { checked: false, id: '4', title: 'Гаражне зберігання', event: '' },
-  { checked: false, id: '5', title: 'Перший власник', event: '' },
-  { checked: false, id: '6', title: 'Сервісна книжка', event: '' },
-  { checked: false, id: '7', title: 'Потребує кузовного ремонту', event: '' },
-  { checked: false, id: '8', title: 'Потребує ремонту ходової', event: '' },
-  { checked: false, id: '9', title: 'Потребує ремонту двигуна', event: '' },
-  { checked: false, id: '10', title: 'Після ДТП', event: '' },
-  { checked: false, id: '11', title: 'Не на ходу', event: '' },
-] as unknown as IFilterCheckboxItem[]
-
-// Комфорт (
-//   - Електропакет
-//   - Електросклопідйомники
-//   - Кондиціонер
-//   - Парктроник
-//   - Бортовий комп'ютер
-//   - Датчик світла
-//   - Запуск кнопкою
-//   - Клімат контроль
-//   - Шкіряний салон
-//   - Круїз контроль
-//   - Люк
-//   - Панорамний дах
-//   - Ел. регулювання сидінь
-//   - Камера заднього виду
-//   - Передня камера
-//   - Мультируль
-//   - Омивач фар
-//   - Підігрів дзеркал
-//   - Обігрів керма
-//   - Підсилювач керма
-//   - Підігрів седінь
-//   - Тонування скла
-//   - Пам'ять сидінь
-//   - Датчик дощу
-//   - Алькантара
-// )
-
-// Комфорт
-const Comfort = [
-  { checked: false, id: '1', title: 'Електропакет', event: '' },
-  { checked: false, id: '2', title: 'Електросклопідйомники', event: '' },
-  { checked: false, id: '3', title: 'Кондиціонер', event: '' },
-  { checked: false, id: '4', title: 'Парктроник', event: '' },
-  { checked: false, id: '5', title: "Бортовий комп'ютер", event: '' },
-  { checked: false, id: '6', title: 'Датчик світла', event: '' },
-  { checked: false, id: '7', title: 'Запуск кнопкою', event: '' },
-  { checked: false, id: '8', title: 'Клімат контроль', event: '' },
-  { checked: false, id: '9', title: 'Шкіряний салон', event: '' },
-  { checked: false, id: '10', title: 'Круїз контроль', event: '' },
-  { checked: false, id: '11', title: 'Люк', event: '' },
-  { checked: false, id: '12', title: 'Панорамний дах', event: '' },
-  { checked: false, id: '13', title: 'Ел. регулювання сидінь', event: '' },
-  { checked: false, id: '14', title: 'Камера заднього виду', event: '' },
-  { checked: false, id: '15', title: 'Передня камера', event: '' },
-  { checked: false, id: '16', title: 'Мультируль', event: '' },
-  { checked: false, id: '17', title: 'Омивач фар', event: '' },
-  { checked: false, id: '18', title: 'Підігрів дзеркал', event: '' },
-  { checked: false, id: '19', title: 'Обігрів керма', event: '' },
-  { checked: false, id: '20', title: 'Підсилювач керма', event: '' },
-  { checked: false, id: '21', title: 'Підігрів седінь', event: '' },
-  { checked: false, id: '22', title: 'Тонування скла', event: '' },
-  { checked: false, id: '23', title: "Пам'ять сидінь", event: '' },
-  { checked: false, id: '24', title: 'Датчик дощу', event: '' },
-  { checked: false, id: '25', title: 'Алькантара', event: '' },
-] as unknown as IFilterCheckboxItem[]
+import spinnerStyles from '@/styles/spinner/index.module.scss'
+import { ICatalogFilterDesktopProps, IFilterCheckboxItem } from '@/types/catalog'
+import { filters } from '@/constans/filter'
 
 const CatalogFiltersDesktop = ({
   priceRange,
@@ -148,32 +68,16 @@ const CatalogFiltersDesktop = ({
         />
       </div>
 
-      <div className={styles.filters__boiler_manufacturers}>
-        <FilterManufacturerAccordion
-          title="Лакове покриття"
-          manufacturersList={varnishCoating}
-          setManufacturer={setPartsManufacturers}
-          updateManufacturer={updatePartsManufacturer}
-        />
-      </div>
-
-      <div className={styles.filters__boiler_manufacturers}>
-        <FilterManufacturerAccordion
-          title="Технічний стан"
-          manufacturersList={technicalCondition}
-          setManufacturer={setPartsManufacturers}
-          updateManufacturer={updatePartsManufacturer}
-        />
-      </div>
-
-      <div className={styles.filters__boiler_manufacturers}>
-        <FilterManufacturerAccordion
-          title="Комфорт"
-          manufacturersList={Comfort}
-          setManufacturer={setPartsManufacturers}
-          updateManufacturer={updatePartsManufacturer}
-        />
-      </div>
+      {filters.map((el) => (
+        <div className={styles.filters__boiler_manufacturers}>
+          <FilterManufacturerAccordion
+            title={el.label}
+            manufacturersList={el.items}
+            setManufacturer={setPartsManufacturers}
+            updateManufacturer={updatePartsManufacturer}
+          />
+        </div>
+      ))}
 
       <div className={styles.filters__actions}>
         <button
---
+++ constans/filter.ts
@@ -0,0 +1,298 @@
+import { IFilterCheckboxItem } from '@/types/catalog'
+
+// Категорія
+export const category = [
+  { checked: false, id: '1', title: 'Легкові автомобілі', event: '' },
+  { checked: false, id: '2', title: 'Вантажні автомобілі', event: '' },
+  { checked: false, id: '3', title: 'Автобуси', event: '' },
+  { checked: false, id: '4', title: 'Мото', event: '' },
+  { checked: false, id: '5', title: 'Інший транспорт', event: '' },
+]
+
+// Виробник
+export const subcategory = [
+  { checked: false, id: '1', title: 'Acura', event: '' },
+  { checked: false, id: '2', title: 'Alfa Romeo', event: '' },
+  { checked: false, id: '3', title: 'Aston Martin', event: '' },
+  { checked: false, id: '4', title: 'Audi', event: '' },
+  { checked: false, id: '5', title: 'Bentley', event: '' },
+  { checked: false, id: '6', title: 'BMW', event: '' },
+  { checked: false, id: '7', title: 'Buick', event: '' },
+  { checked: false, id: '8', title: 'Cadillac', event: '' },
+  { checked: false, id: '9', title: 'Chana', event: '' },
+  { checked: false, id: '10', title: 'Chery', event: '' },
+  { checked: false, id: '11', title: 'Chevrolet', event: '' },
+  { checked: false, id: '12', title: 'Chrysler', event: '' },
+  { checked: false, id: '13', title: 'Citroen', event: '' },
+  { checked: false, id: '14', title: 'Dacia', event: '' },
+  { checked: false, id: '15', title: 'Daewoo', event: '' },
+  { checked: false, id: '16', title: 'Dodge', event: '' },
+  { checked: false, id: '17', title: 'Ferrari', event: '' },
+  { checked: false, id: '18', title: 'Fiat', event: '' },
+  { checked: false, id: '19', title: 'Ford', event: '' },
+  { checked: false, id: '20', title: 'Geely', event: '' },
+  { checked: false, id: '21', title: 'Honda', event: '' },
+  { checked: false, id: '22', title: 'Hummer', event: '' },
+  { checked: false, id: '23', title: 'Huyndai', event: '' },
+  { checked: false, id: '24', title: 'Infinity', event: '' },
+  { checked: false, id: '25', title: 'Jaguar', event: '' },
+  { checked: false, id: '26', title: 'Jeep', event: '' },
+  { checked: false, id: '27', title: 'Kia', event: '' },
+  { checked: false, id: '28', title: 'Lamborghini', event: '' },
+  { checked: false, id: '29', title: 'Land Rover', event: '' },
+  { checked: false, id: '30', title: 'Lexus', event: '' },
+  { checked: false, id: '31', title: 'Lifan', event: '' },
+  { checked: false, id: '32', title: 'Lincoln', event: '' },
+  { checked: false, id: '33', title: 'Maserati', event: '' },
+  { checked: false, id: '34', title: 'Maybach', event: '' },
+  { checked: false, id: '35', title: 'Mazda', event: '' },
+  { checked: false, id: '36', title: 'McLaren', event: '' },
+  { checked: false, id: '37', title: 'Mercedes-Menz', event: '' },
+  { checked: false, id: '38', title: 'Mitsubishi', event: '' },
+  { checked: false, id: '39', title: 'Nissan', event: '' },
+  { checked: false, id: '40', title: 'Opel', event: '' },
+  { checked: false, id: '41', title: 'Peugeot', event: '' },
+  { checked: false, id: '42', title: 'Porsche', event: '' },
+  { checked: false, id: '43', title: 'Renault', event: '' },
+  { checked: false, id: '44', title: 'Rolls-Royse', event: '' },
+  { checked: false, id: '45', title: 'Rover', event: '' },
+  { checked: false, id: '46', title: 'Skoda', event: '' },
+  { checked: false, id: '47', title: 'Subaru', event: '' },
+  { checked: false, id: '48', title: 'Suzuki', event: '' },
+  { checked: false, id: '49', title: 'Tesla', event: '' },
+  { checked: false, id: '50', title: 'Toyota', event: '' },
+  { checked: false, id: '51', title: 'Volkswagen', event: '' },
+  { checked: false, id: '52', title: 'Volvo', event: '' },
+  { checked: false, id: '53', title: 'Інші', event: '' },
+]
+
+// Тип автомобіля
+export const carType = [
+  { checked: false, id: '1', title: 'Всі оголошення', event: '' },
+  { checked: false, id: '2', title: 'З пробігом', event: '' },
+  { checked: false, id: '3', title: 'Новий', event: '' },
+  { checked: false, id: '4', title: 'Під пригон (авто не в Україні)', event: '' },
+]
+
+// Коробка передач
+export const gearbox = [
+  { checked: false, id: '1', title: 'Механічна', event: '' },
+  { checked: false, id: '2', title: 'Автоматична', event: '' },
+  { checked: false, id: '3', title: 'Варіатор', event: '' },
+  { checked: false, id: '4', title: 'Адаптивна', event: '' },
+  { checked: false, id: '5', title: 'Типтронік', event: '' },
+]
+
+// Тип палива
+export const fuelType = [
+  { checked: false, id: '1', title: 'Бензин', event: '' },
+  { checked: false, id: '2', title: 'Дизель', event: '' },
+  { checked: false, id: '3', title: 'Газ / бензин', event: '' },
+  { checked: false, id: '4', title: 'Електро', event: '' },
+  { checked: false, id: '5', title: 'Гібрид', event: '' },
+  { checked: false, id: '6', title: 'Інший', event: '' },
+]
+
+// Тип кузова
+export const carBodyType = [
+  { checked: false, id: '1', title: 'Кабріолет', event: '' },
+  { checked: false, id: '2', title: 'Пікап', event: '' },
+  { checked: false, id: '3', title: 'Купе', event: '' },
+  { checked: false, id: '4', title: 'Універсал', event: '' },
+  { checked: false, id: '5', title: 'Хетчбек', event: '' },
+  { checked: false, id: '6', title: 'Мінівен', event: '' },
+  { checked: false, id: '7', title: 'Позашляховик / Кроссовер', event: '' },
+  { checked: false, id: '8', title: 'Седан', event: '' },
+  { checked: false, id: '9', title: 'Ліфтбек', event: '' },
+  { checked: false, id: '10', title: 'Лімузин', event: '' },
+  { checked: false, id: '11', title: 'Інший', event: '' },
+]
+
+// Кількість місць
+export const seatsCount = [
+  { checked: false, id: '1', title: '2', event: '' },
+  { checked: false, id: '2', title: '3', event: '' },
+  { checked: false, id: '3', title: '4', event: '' },
+  { checked: false, id: '4', title: '5', event: '' },
+  { checked: false, id: '5', title: '6', event: '' },
+  { checked: false, id: '6', title: '7', event: '' },
+  { checked: false, id: '7', title: '8 і більше', event: '' },
+]
+
+// Тип приводу
+export const driveType = [
+  { checked: false, id: '1', title: 'Повний', event: '' },
+  { checked: false, id: '2', title: 'Передній', event: '' },
+  { checked: false, id: '3', title: 'Задній', event: '' },
+]
+
+// Об'єм двигуна
+export const engineVolume = [
+  { checked: false, id: '1', title: '1 л.', event: '' },
+  { checked: false, id: '2', title: '2 л.', event: '' },
+  { checked: false, id: '3', title: '3 л.', event: '' },
+  { checked: false, id: '4', title: '5 л.', event: '' },
+  { checked: false, id: '5', title: '7 л.', event: '' },
+]
+
+// Колір
+export const color = [
+  { checked: false, id: '1', title: 'Білий', event: '' },
+  { checked: false, id: '2', title: 'Чорний', event: '' },
+  { checked: false, id: '3', title: 'Синій', event: '' },
+  { checked: false, id: '4', title: 'Сірий', event: '' },
+  { checked: false, id: '5', title: 'Сріблястий', event: '' },
+  { checked: false, id: '6', title: 'Червоний', event: '' },
+  { checked: false, id: '7', title: 'Зелений', event: '' },
+  { checked: false, id: '8', title: 'Бежевий', event: '' },
+  { checked: false, id: '9', title: 'Бірюзовий', event: '' },
+  { checked: false, id: '10', title: 'Бронзовий', event: '' },
+  { checked: false, id: '11', title: 'Вишневий', event: '' },
+  { checked: false, id: '12', title: 'Блакитний', event: '' },
+  { checked: false, id: '13', title: 'Жовтий', event: '' },
+  { checked: false, id: '14', title: 'Золотистий', event: '' },
+  { checked: false, id: '15', title: 'Коричневий', event: '' },
+  { checked: false, id: '16', title: 'Матовий', event: '' },
+  { checked: false, id: '17', title: 'Оливковий', event: '' },
+  { checked: false, id: '18', title: 'Рожевий', event: '' },
+  { checked: false, id: '19', title: 'Фіолетовий', event: '' },
+  { checked: false, id: '20', title: 'Інший', event: '' },
+] as unknown as IFilterCheckboxItem[]
+
+// Лакове покриття
+export const varnishCoating = [
+  { checked: false, id: '1', title: 'Як нове, без видимих слідів експлуатації', event: '' },
+  { checked: false, id: '2', title: 'Професійно відремонтоварі сліди експлуатації', event: '' },
+  { checked: false, id: '3', title: 'Незначні сліди експлуатації (дрібні подряпини, сколи)', event: '' },
+  { checked: false, id: '4', title: "Не відремонтовані сліди експлуатації (подряпини, вм'ятини і т.д.)", event: '' },
+  { checked: false, id: '5', title: 'Потрібно відновлення (фарбування, заміна деталей, зварювання)', event: '' },
+] as unknown as IFilterCheckboxItem[]
+
+// Технічний стан
+export const technicalCondition = [
+  { checked: false, id: '1', title: 'На ходу, технічно справна', event: '' },
+  { checked: false, id: '2', title: 'Не бита', event: '' },
+  { checked: false, id: '3', title: 'Не пофарбована', event: '' },
+  { checked: false, id: '4', title: 'Гаражне зберігання', event: '' },
+  { checked: false, id: '5', title: 'Перший власник', event: '' },
+  { checked: false, id: '6', title: 'Сервісна книжка', event: '' },
+  { checked: false, id: '7', title: 'Потребує кузовного ремонту', event: '' },
+  { checked: false, id: '8', title: 'Потребує ремонту ходової', event: '' },
+  { checked: false, id: '9', title: 'Потребує ремонту двигуна', event: '' },
+  { checked: false, id: '10', title: 'Після ДТП', event: '' },
+  { checked: false, id: '11', title: 'Не на ходу', event: '' },
+] as unknown as IFilterCheckboxItem[]
+
+// Комфорт
+export const comfort = [
+  { checked: false, id: '1', title: 'Електропакет', event: '' },
+  { checked: false, id: '2', title: 'Електросклопідйомники', event: '' },
+  { checked: false, id: '3', title: 'Кондиціонер', event: '' },
+  { checked: false, id: '4', title: 'Парктроник', event: '' },
+  { checked: false, id: '5', title: "Бортовий комп'ютер", event: '' },
+  { checked: false, id: '6', title: 'Датчик світла', event: '' },
+  { checked: false, id: '7', title: 'Запуск кнопкою', event: '' },
+  { checked: false, id: '8', title: 'Клімат контроль', event: '' },
+  { checked: false, id: '9', title: 'Шкіряний салон', event: '' },
+  { checked: false, id: '10', title: 'Круїз контроль', event: '' },
+  { checked: false, id: '11', title: 'Люк', event: '' },
+  { checked: false, id: '12', title: 'Панорамний дах', event: '' },
+  { checked: false, id: '13', title: 'Ел. регулювання сидінь', event: '' },
+  { checked: false, id: '14', title: 'Камера заднього виду', event: '' },
+  { checked: false, id: '15', title: 'Передня камера', event: '' },
+  { checked: false, id: '16', title: 'Мультируль', event: '' },
+  { checked: false, id: '17', title: 'Омивач фар', event: '' },
+  { checked: false, id: '18', title: 'Підігрів дзеркал', event: '' },
+  { checked: false, id: '19', title: 'Обігрів керма', event: '' },
+  { checked: false, id: '20', title: 'Підсилювач керма', event: '' },
+  { checked: false, id: '21', title: 'Підігрів седінь', event: '' },
+  { checked: false, id: '22', title: 'Тонування скла', event: '' },
+  { checked: false, id: '23', title: "Пам'ять сидінь", event: '' },
+  { checked: false, id: '24', title: 'Датчик дощу', event: '' },
+  { checked: false, id: '25', title: 'Алькантара', event: '' },
+] as unknown as IFilterCheckboxItem[]
+
+// Мультимедіа (
+export const multimedia = [
+  { checked: false, id: '1', title: 'CD', event: '' },
+  { checked: false, id: '2', title: 'AUX', event: '' },
+  { checked: false, id: '3', title: 'Bluetooth', event: '' },
+  { checked: false, id: '4', title: 'USB', event: '' },
+  { checked: false, id: '5', title: 'Акустика', event: '' },
+  { checked: false, id: '6', title: 'Магнітола', event: '' },
+  { checked: false, id: '7', title: 'Підсилювач', event: '' },
+  { checked: false, id: '8', title: 'Сабвуфер', event: '' },
+  { checked: false, id: '9', title: 'Система навігації GPS', event: '' },
+  { checked: false, id: '10', title: 'Apple CarPay', event: '' },
+  { checked: false, id: '11', title: 'Android Auto', event: '' },
+]
+
+// Безпека
+export const security = [
+  { checked: false, id: '1', title: 'ABD', event: '' },
+  { checked: false, id: '2', title: 'ABS', event: '' },
+  { checked: false, id: '3', title: 'ESP', event: '' },
+  { checked: false, id: '4', title: 'Броньований автомобіль', event: '' },
+  { checked: false, id: '5', title: 'Галогенні фари', event: '' },
+  { checked: false, id: '6', title: 'Замок на КПП', event: '' },
+  { checked: false, id: '7', title: 'Іммобілайзер', event: '' },
+  { checked: false, id: '8', title: 'Пневмопідвіска', event: '' },
+  { checked: false, id: '9', title: 'Подушка безпеки (Airbag)', event: '' },
+  { checked: false, id: '10', title: 'Сервокермо', event: '' },
+  { checked: false, id: '11', title: 'Сигналізація', event: '' },
+  { checked: false, id: '12', title: 'Центральний замок', event: '' },
+  { checked: false, id: '13', title: 'LED фари', event: '' },
+  { checked: false, id: '14', title: 'Лазерні фари', event: '' },
+  { checked: false, id: '15', title: 'Кснон', event: '' },
+  { checked: false, id: '16', title: 'Денні ходові вогні', event: '' },
+  { checked: false, id: '17', title: 'GPS трекер', event: '' },
+  { checked: false, id: '18', title: 'Безключовий доступ', event: '' },
+]
+
+// Розмитнена
+export const сustomsСleared = [
+  { checked: false, id: '1', title: 'Так', event: '' },
+  { checked: false, id: '2', title: 'Ні', event: '' },
+  { checked: false, id: '3', title: 'Розмитнення "під ключ"', event: '' },
+]
+
+// Авто пригнано з
+export const theCarWasDrivenFrom = [
+  { checked: false, id: '1', title: 'США', event: '' },
+  { checked: false, id: '2', title: 'Німеччина', event: '' },
+  { checked: false, id: '3', title: 'Литва', event: '' },
+  { checked: false, id: '4', title: 'Грузія', event: '' },
+  { checked: false, id: '5', title: 'Корея', event: '' },
+  { checked: false, id: '6', title: 'Польша', event: '' },
+  { checked: false, id: '7', title: 'Швейцарія', event: '' },
+  { checked: false, id: '8', title: 'Норвегія', event: '' },
+  { checked: false, id: '9', title: 'Франція', event: '' },
+  { checked: false, id: '10', title: 'Молдова', event: '' },
+  { checked: false, id: '11', title: 'Іспанія', event: '' },
+  { checked: false, id: '12', title: 'Італія', event: '' },
+  { checked: false, id: '13', title: 'Португалія', event: '' },
+  { checked: false, id: '14', title: 'Нідерланди', event: '' },
+  { checked: false, id: '15', title: 'Бельгія', event: '' },
+  { checked: false, id: '16', title: 'Латвія', event: '' },
+  { checked: false, id: '16', title: 'Інша країна', event: '' },
+]
+
+export const filters = [
+  { label: 'Категорія', items: category },
+  { label: 'Виробник', items: subcategory },
+  { label: 'Тип автомобіля', items: carType },
+  { label: 'Коробка передач', items: gearbox },
+  { label: 'Тип палива', items: fuelType },
+  { label: 'Тип кузова', items: carBodyType },
+  { label: 'Кількість місць', items: seatsCount },
+  { label: 'Тип приводу', items: driveType },
+  { label: "Об'єм двигуна", items: engineVolume },
+  { label: 'Колір', items: color },
+  { label: 'Лакове покриття', items: varnishCoating },
+  { label: 'Технічний стан', items: technicalCondition },
+  { label: 'Комфорт', items: comfort },
+  { label: 'Мультимедіа', items: multimedia },
+  { label: 'Безпека', items: security },
+  { label: 'Розмитнена', items: сustomsСleared },
+  { label: 'Авто пригнано з', items: theCarWasDrivenFrom },
+]
