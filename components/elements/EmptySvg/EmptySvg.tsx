ab246ad6e36363afd3bbb5b22b4ad39445a0670c
Roman2508 <2508romaa@gmail.com>
2024-11-21 15:45:31 +0200

    _fix

--- components/elements/EmptySvg/EmptySvg.tsx
+++ components/elements/EmptySvg/EmptySvg.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react'
 
 const Empty = ({ darkModeClass, text = 'Пусто...' }: { darkModeClass: string; text?: string }) => {
--- components/elements/GarbageSvg/GarbageSvg.tsx
+++ components/elements/GarbageSvg/GarbageSvg.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react'
 
 const GarbageSvg = ({ darkModeClass }: { darkModeClass: string }) => {
